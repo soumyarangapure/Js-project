e8e1d850e22020b580171e7ee5706244c9a4bdb7
soumyarangapure <soumyarangapure@gmail.com>
2025-05-03 10:32:36 +0000

    container config

--- .devcontainer/Assignment/script.js
+++ .devcontainer/Assignment/script.js
@@ -24,7 +24,7 @@ function login() {
 }
 
 function loadUsers() {
-  fetch(https://reqres.in/api/users?page=${currentPage++}).then(res => res.json()).then(data => {const container = document.getElementById('usersContainer');
+  fetch(`https://reqres.in/api/users?page=${currentPage++}`).then(res => res.json()).then(data => {const container = document.getElementById('usersContainer');
       data.data.forEach(user => {
         const div = document.createElement('div');
         div.className = 'user-card';
@@ -43,24 +43,25 @@ function loadUsers() {
 }
 
 function editUser(id, firstName, lastName, email) {
-  const newName = prompt("Edit Name:", ${firstName} ${lastName});
+  const newName = prompt(`Edit Name:, ${firstName} ${lastName}`);
   const newEmail = prompt("Edit Email:", email);
   if (!newName || !newEmail) return;
 
-  const [first, last] = newName.split(" ");
-  fetch(https://reqres.in/api/users/${id}, {
+  const [first, last=""] = newName.split(" ");
+  fetch(`https://reqres.in/api/users/${id}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ first_name: first, last_name: last, email: newEmail })
-  })
+
   .then(res => res.json())
-  .then(data => alert("User updated (simulated): " + JSON.stringify(data)));
+  .then(data => alert("User updated (simulated): " + JSON.stringify(data)))
+  });
 
 function deleteUser(id, btn) {
-  fetch(https://reqres.in/api/users/${id},
-     { method: 'DELETE' })
+  fetch(`https://reqres.in/api/users/${id}`,{ method: 'DELETE' })
     .then(() => {
       btn.closest('.user-card').remove();
       alert("User deleted (simulated)");
     });
+}
 }
